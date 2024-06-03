e7f63ec39b1a1127d912282c72077b3b06562e35
nnnnnnnao3 <165908897+nnnnnnnao3@users.noreply.github.com>
2024-06-03 22:47:38 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name,     presence: true
+ 
+  validates :password, format: { 
+    with: /\A(?=.*?[a-zA-Z])(?=.*?\d)[a-zA-Z\d]+\z/, 
+    message: "must include at least one letter and one digit" 
+  }
+  has_many :tasks
 end
